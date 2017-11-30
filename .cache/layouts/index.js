901906f315af45185fef0bdaff7455c6f1891772
DanielNoonan <dannoonan99@gmail.com>
2017-11-30 13:10:10 +0000

    Initital Commit - minimum gatsby starter files.

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "C:/Users/danno/Documents/webDev/my-websites/SkyParkFarm/SkyParkFarmPWA/src/layouts/index.js"
+  import data from "C:\\Users\\danno\\Documents\\webDev\\my-websites\\SkyParkFarm\\SkyParkFarmPWA\\.cache\\json\\layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
---
+++ .cache/sync-requires.js
@@ -0,0 +1,27 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\danno\\Documents\\webDev\\my-websites\\SkyParkFarm\\SkyParkFarmPWA\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("C:\\Users\\danno\\Documents\\webDev\\my-websites\\SkyParkFarm\\SkyParkFarmPWA\\src\\pages\\404.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\danno\\Documents\\webDev\\my-websites\\SkyParkFarm\\SkyParkFarmPWA\\src\\pages\\index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("C:\\Users\\danno\\Documents\\webDev\\my-websites\\SkyParkFarm\\SkyParkFarmPWA\\src\\pages\\page-2.js"))
+}
+
+exports.json = {
+  "layout-index.json": require("C:\\Users\\danno\\Documents\\webDev\\my-websites\\SkyParkFarm\\SkyParkFarmPWA\\.cache\\json\\layout-index.json"),
+  "dev-404-page.json": require("C:\\Users\\danno\\Documents\\webDev\\my-websites\\SkyParkFarm\\SkyParkFarmPWA\\.cache\\json\\dev-404-page.json"),
+  "layout-index.json": require("C:\\Users\\danno\\Documents\\webDev\\my-websites\\SkyParkFarm\\SkyParkFarmPWA\\.cache\\json\\layout-index.json"),
+  "404.json": require("C:\\Users\\danno\\Documents\\webDev\\my-websites\\SkyParkFarm\\SkyParkFarmPWA\\.cache\\json\\404.json"),
+  "layout-index.json": require("C:\\Users\\danno\\Documents\\webDev\\my-websites\\SkyParkFarm\\SkyParkFarmPWA\\.cache\\json\\layout-index.json"),
+  "index.json": require("C:\\Users\\danno\\Documents\\webDev\\my-websites\\SkyParkFarm\\SkyParkFarmPWA\\.cache\\json\\index.json"),
+  "layout-index.json": require("C:\\Users\\danno\\Documents\\webDev\\my-websites\\SkyParkFarm\\SkyParkFarmPWA\\.cache\\json\\layout-index.json"),
+  "404-html.json": require("C:\\Users\\danno\\Documents\\webDev\\my-websites\\SkyParkFarm\\SkyParkFarmPWA\\.cache\\json\\404-html.json"),
+  "layout-index.json": require("C:\\Users\\danno\\Documents\\webDev\\my-websites\\SkyParkFarm\\SkyParkFarmPWA\\.cache\\json\\layout-index.json"),
+  "page-2.json": require("C:\\Users\\danno\\Documents\\webDev\\my-websites\\SkyParkFarm\\SkyParkFarmPWA\\.cache\\json\\page-2.json")
+}
+
+exports.layouts = {
+  "component---src-layouts-index-js": preferDefault(require("C:/Users/danno/Documents/webDev/my-websites/SkyParkFarm/SkyParkFarmPWA/.cache/layouts/index.js"))
+}
