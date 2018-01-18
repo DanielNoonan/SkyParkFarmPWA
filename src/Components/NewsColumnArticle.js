37699cce9a098f99b5010d61bb0b01cbc54f1c78
Dan Noonan <dannoonan@Dans-Air.home>
2018-01-18 15:58:28 +0000

    Complete implementaion of a responsive image working.

--- src/Components/NewsColumnArticle.js
+++ src/Components/NewsColumnArticle.js
@@ -9,6 +9,9 @@ const NewsColumnArticle = (props) => {
             <h2 onClick={props.clicked}>&#8656; Front Page Story</h2>
             <NewsArticleHeadline>{props.title}</NewsArticleHeadline>
             <NewsArticleImage>
+                <source media='(min-width: 1800px)' srcSet={`${props.image}?w=1800`} alt='source Dummy Alt Text' />
+                <source media='(min-width: 1200px)' srcSet={`${props.image}?w=1200`} alt='source Dummy Alt Text' />
+                <source media='(min-width: 900px)' srcSet={`${props.image}?w=900`} alt='source Dummy Alt Text' />
                 <source media='(min-width: 650px)' srcSet={`${props.image}?w=650`} alt='source Dummy Alt Text' />
                 <img src={props.image} alt='img Dummy Alt Text' />
             </NewsArticleImage>
