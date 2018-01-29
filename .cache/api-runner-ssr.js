e45c1828ad7903d6bcdbb42a6b869e3cbf7bbff1
Daniel Noonan <dannoonan99@gmail.com>
2018-01-29 12:45:49 +0000

    New NPM install

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,8 +1,8 @@
 var plugins = [{
-      plugin: require('/Users/dannoonan/Desktop/webDev/SkyParkFarm/SkyParkFarmPWA--gitRepo/node_modules/gatsby-plugin-react-helmet/gatsby-ssr.js'),
+      plugin: require('C:/Users/danno/Documents/webDev/SkyParkFarm/SkyParkFarmPWA--gitRepo/node_modules/gatsby-plugin-react-helmet/gatsby-ssr.js'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Users/dannoonan/Desktop/webDev/SkyParkFarm/SkyParkFarmPWA--gitRepo/node_modules/gatsby-plugin-styled-components/gatsby-ssr.js'),
+      plugin: require('C:/Users/danno/Documents/webDev/SkyParkFarm/SkyParkFarmPWA--gitRepo/node_modules/gatsby-plugin-styled-components/gatsby-ssr.js'),
       options: {"plugins":[]},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
--- .cache/layouts/index.js
+++ .cache/layouts/index.js
@@ -1,7 +1,7 @@
 
   import React from "react"
-  import Component from "/Users/dannoonan/Desktop/webDev/SkyParkFarm/SkyParkFarmPWA--gitRepo/src/layouts/index.js"
-  import data from "/Users/dannoonan/Desktop/webDev/SkyParkFarm/SkyParkFarmPWA--gitRepo/.cache/json/layout-index.json"
+  import Component from "C:/Users/danno/Documents/webDev/SkyParkFarm/SkyParkFarmPWA--gitRepo/src/layouts/index.js"
+  import data from "C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\.cache\\json\\layout-index.json"
 
   export default (props) => <Component {...props} {...data} />
   
