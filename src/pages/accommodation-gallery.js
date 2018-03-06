71b3d1698ef014d983049bf76f122954d7e418bc
Daniel Noonan <dannoonan99@gmail.com>
2018-03-06 13:52:54 +0000

    Edited a comment

--- src/pages/accommodation-gallery.js
+++ src/pages/accommodation-gallery.js
@@ -36,7 +36,7 @@ class AccommodationGallery extends Component {
 export default AccommodationGallery;
 
 //note: query Contentful for carousel-images/text/page-images --- pass this data as a prop to AccommodationGalleryContainer --- pass this.props.data to GalleryCarousel component --- render images in carousel component using .map()  
-// page > container > components
+// page > container > component
 
 
 
