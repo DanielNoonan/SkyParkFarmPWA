c8eae356e93762e4a1b97862a568d50e2e8749d5
Daniel Noonan <dannoonan99@gmail.com>
2018-04-20 12:39:11 +0100

    Location page with google maps api and responsive styling added.

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -7,9 +7,9 @@ exports.components = {
   "component---src-pages-accommodation-gallery-js": require("gatsby-module-loader?name=component---src-pages-accommodation-gallery-js!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\src\\pages\\accommodation-gallery.js"),
   "component---src-pages-bookings-accommodation-js": require("gatsby-module-loader?name=component---src-pages-bookings-accommodation-js!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\src\\pages\\bookings-accommodation.js"),
   "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\src\\pages\\index.js"),
+  "component---src-pages-map-contact-js": require("gatsby-module-loader?name=component---src-pages-map-contact-js!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\src\\pages\\map-contact.js"),
   "component---src-pages-news-archive-js": require("gatsby-module-loader?name=component---src-pages-news-archive-js!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\src\\pages\\news-archive.js"),
-  "component---src-pages-page-2-js": require("gatsby-module-loader?name=component---src-pages-page-2-js!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\src\\pages\\page-2.js"),
-  "component---src-pages-map-contact-js": require("gatsby-module-loader?name=component---src-pages-map-contact-js!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\src\\pages\\map-contact.js")
+  "component---src-pages-page-2-js": require("gatsby-module-loader?name=component---src-pages-page-2-js!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\src\\pages\\page-2.js")
 }
 
 exports.json = {
@@ -24,13 +24,13 @@ exports.json = {
   "layout-index.json": require("gatsby-module-loader?name=path---!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\.cache\\json\\layout-index.json"),
   "index.json": require("gatsby-module-loader?name=path---index!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\.cache\\json\\index.json"),
   "layout-index.json": require("gatsby-module-loader?name=path---!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\.cache\\json\\layout-index.json"),
+  "map-contact.json": require("gatsby-module-loader?name=path---map-contact!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\.cache\\json\\map-contact.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\.cache\\json\\layout-index.json"),
   "news-archive.json": require("gatsby-module-loader?name=path---news-archive!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\.cache\\json\\news-archive.json"),
   "layout-index.json": require("gatsby-module-loader?name=path---!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\.cache\\json\\layout-index.json"),
   "page-2.json": require("gatsby-module-loader?name=path---page-2!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\.cache\\json\\page-2.json"),
   "layout-index.json": require("gatsby-module-loader?name=path---!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\.cache\\json\\layout-index.json"),
-  "404-html.json": require("gatsby-module-loader?name=path---404-html!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\.cache\\json\\404-html.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\.cache\\json\\layout-index.json"),
-  "map-contact.json": require("gatsby-module-loader?name=path---map-contact!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\.cache\\json\\map-contact.json")
+  "404-html.json": require("gatsby-module-loader?name=path---404-html!C:\\Users\\danno\\Documents\\webDev\\SkyParkFarm\\SkyParkFarmPWA--gitRepo\\.cache\\json\\404-html.json")
 }
 
 exports.layouts = {
--- src/pages/map-contact.js
+++ src/pages/map-contact.js
@@ -8,24 +8,106 @@ const MapContact = () => {
     }
 
     return (
-        <div>
-            <MapContainer>
+        <PageContainer>
                 <iframe style={style} width="100%" height="450" frameBorder="0" src="https://www.google.com/maps/embed/v1/place?q=50.999061%2C-0.8874504&key=AIzaSyDAbO9F8qrwx2Ab0se4-00kCJnTUlrJtwE&zoom=12" allowFullScreen></iframe>
-            </MapContainer>
-            {/* <h1>HI FROM THE MAP-CONTACT PAGE</h1>
-            <p>Welcome to page 2</p>
-            <Link to="/">Go back to the homepage</Link> */}
-        </div>
+            <DirectionsContainer>
+                <DirectionsBox>
+                    <DirectionsHeading>Directions from the A3 (3 miles)</DirectionsHeading>
+                    <DirectionsText>
+                        <Theta>&#920; </Theta>
+                        Leave A3 for A272 signposted Midhurst.
+                    </DirectionsText>
+                    <DirectionsText>
+                        <Theta>&#920; </Theta>
+                        Continue 0.3 miles to the Roundabout - take first exit.
+                    </DirectionsText>
+                    <DirectionsText>
+                        <Theta>&#920; </Theta>
+                        After 0.7 miles turn right, across the dual-carriageway to continue on the A272 (after turning you will drive past a fallen tree on the corner to your left).
+                    </DirectionsText>
+                    <DirectionsText>
+                        <Theta>&#920; </Theta>
+                        After 1.3 miles turn right onto Durford Lane.
+                    </DirectionsText>
+                    <DirectionsText>
+                        <Theta>&#920; </Theta>
+                        Continue for 0.5 miles - Welcome to Sky Park Farm!
+                    </DirectionsText>
+                </DirectionsBox>
+                <DirectionsBox>
+                    <DirectionsHeading>Directions from Midhurst (7.6 miles)</DirectionsHeading>
+                    <DirectionsText>
+                        <Theta>&#920; </Theta>
+                        Leave Midhurst on the A272 Petersfield Rd.
+                    </DirectionsText>
+                    <DirectionsText>
+                        <Theta>&#920; </Theta>
+                        After 3 miles you will pass Trotton &amp; Aylings Garden Center.
+                    </DirectionsText>
+                    <DirectionsText>
+                        <Theta>&#920; </Theta>
+                        After another 3 miles you will pass through the small village of Rogate.
+                    </DirectionsText>
+                    <DirectionsText>
+                        <Theta>&#920; </Theta>
+                        After another 1.1 miles turn left onto Durford Lane.
+                    </DirectionsText>
+                    <DirectionsText>
+                        <Theta>&#920; </Theta>
+                        Continue for 0.5 miles - Welcome to Sky Park Farm!
+                    </DirectionsText>
+                </DirectionsBox>
+            </DirectionsContainer>
+        </PageContainer>
     )
 }
 
 export default MapContact;
 
 //Styled Components
-const MapContainer = styled.div`
+const PageContainer = styled.div`
+    background: #f6f6f6;
+    height: 100%;
+`
+
+const DirectionsContainer = styled.div`
+    border: 5px solid #000034;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    background: #F6F6F6;
+    width: 100%;
+`
+const DirectionsBox = styled.div`
+    flex: 0 0 98%;
+    padding: 0 1%;
+    border: 3px solid #80D4F7;
+    color: #404040;
+    font-size: 3em;
+    margin: 20px 0;
+
+    @media (min-width: 1024px) {
+        flex: 0 0 calc(100% / 12 * 5);
+    }
+`
+
+const DirectionsHeading = styled.p`
+    margin: 25px 0;
+    color: #000034;
+    text-align: center;
+    text-decoration: underline;
+`
+
+const DirectionsText = styled.p`
+    font-size: 0.8em;
+`
+
+const Theta = styled.span`
+    color: #BC7817;
+    font-size: 1.1em;
 `
 
 
-{/*Orignal iframe:
+{/*Orignal google maps iframe:
 <iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=50.999061%2C-0.8874504&key=AIzaSyDAbO9F8qrwx2Ab0se4-00kCJnTUlrJtwE" allowfullscreen></iframe> 
 */}
