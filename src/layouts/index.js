da157ab410cbfd89fb37d6a25d615d91ac59a432
Daniel Noonan <dannoonan99@gmail.com>
2018-04-23 12:00:02 +0100

    Force moving Footer.js as git is incorrectly tracking it.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import Navbar from '../Components/Navbar/Navbar';
-import Footer from '../Components/footer';
+import Footer from '../Components/Footer';
 
 
 import '../assets/global-styles/global-styles.css'
